1dd08e5ca9b042334625017c6340af9f974a92aa
yuanqiuye <chenhsinyuan@gmail.com>
2024-01-05 04:23:37 +0800

    fix

--- node_app/module/bss.js
+++ node_app/module/bss.js
@@ -125,6 +125,7 @@ class BSS {
     }
 
     async sendApply(cid, uid, start, end){ 
+        const now = new Date()
         const year = now.getFullYear();
         const month = (now.getMonth() + 1).toString().padStart(2, '0');
         const day = now.getDate().toString().padStart(2, '0');
